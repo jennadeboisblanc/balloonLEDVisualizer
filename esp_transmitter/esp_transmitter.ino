bb3d133573fda6043bf884481e3578300d718e76
jdeboi <jdeboi@gmail.com>
2017-11-18 15:30:27 -0600

    i dunno. something with setting bits is not right

--- esp_transmitter/esp_transmitter.ino
+++ esp_transmitter/esp_transmitter.ino
@@ -1,3 +1,5 @@
+#define BALLOON_NUM 0
+
 #include <painlessMesh.h>
 #define   MESH_SSID       "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
@@ -52,25 +54,50 @@ void setup() {
 
 }
 void loop() {
-  rainbowCycle(100);
   mesh.update();
-
 }
 
 
 void sendMessage() {
+  rainbowCycle(1);
   //  int rgb_msg = random(0, 255);
   //  String msg = String(rgb_msg);
   String msg = "";
-  
+
   for (int i = 0; i < 30; i++) {
     String val = String(balloons[i], HEX);
     msg += val;
   }
-
-  Serial.printf("Sending message: %s\n", msg.c_str());
+  String sending = msg.c_str();
+  Serial.printf("IND: %i, R: %i, G: %i, B: %i \n", rainbowIndex, getRed(sending), getGreen(sending), getBlue(sending));
+  //Serial.printf("Sending message: %s\n", msg.c_str());
   bool error = mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval(100);  // between 1 and 5 seconds
+  taskSendMessage.setInterval(500);  // between 1 and 5 seconds
+}
+
+
+int getRed(String incomingString) {
+  char val[2];
+  val[0] = incomingString[BALLOON_NUM * 3 * 2];
+  val[1] = incomingString[BALLOON_NUM * 3 * 2 + 1];
+  int r = strtol( &val[0], NULL, 16);
+  return r;
+}
+
+int getGreen(String incomingString) {
+  char val[2];
+  val[0] = incomingString[BALLOON_NUM * 3 * 2 + 2];
+  val[1] = incomingString[BALLOON_NUM * 3 * 2 + 3];
+  int g = strtol( &val[0], NULL, 16);
+  return g;
+}
+
+int getBlue(String incomingString) {
+  char val[2];
+  val[0] = incomingString[BALLOON_NUM * 3 * 2 + 4];
+  val[1] = incomingString[BALLOON_NUM * 3 * 2 + 5];
+  int b = strtol( &val[0], NULL, 16);
+  return b;
 }
 
 void setAllBalloons(int r, int g, int b) {
@@ -95,12 +122,9 @@ void setBalloon(byte ind, byte r, byte g, byte b) {
 
 
 // Adapted from Adafruit Neopixel
-void rainbowCycle(uint8_t wait) {
-  if (millis() - lastChecked > wait) {
-    lastChecked = millis();
-    rainbowIndex++;
-    if (rainbowIndex > 256) rainbowIndex = 0;
-  }
+void rainbowCycle(int amt) {
+  rainbowIndex+= amt;
+  if (rainbowIndex > 255) rainbowIndex = 0;
   for (int i = 0; i < numBalloons; i++) {
     byte r =  WheelR(((i * 256 / numBalloons) + rainbowIndex) & 255);
     byte g =  WheelG(((i * 256 / numBalloons) + rainbowIndex) & 255);
@@ -109,21 +133,43 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
-// Adafruit Neopixel Strandtest
-byte WheelR(byte WheelPos) {
+void setAllBalloons(int r, int g, int b) {
+  for (int i = 0; i < numBalloons; i++) {
+    setBalloon(i, r, g, b);
+  }
+}
+
+//void rainbowCycle(uint8_t wait) {
+//  if (millis() - lastChecked > wait) {
+//    lastChecked = millis();
+//    rainbowIndex++;
+//    if (rainbowIndex > 256) rainbowIndex = 0;
+//  }
+//  for (int i = 0; i < numBalloons; i++) {
+//    byte r =  WheelR(((i * 256 / numBalloons) + rainbowIndex) & 255);
+//    byte g =  WheelG(((i * 256 / numBalloons) + rainbowIndex) & 255);
+//    byte b = WheelB(((i * 256 / numBalloons) + rainbowIndex) & 255);
+//    setBalloon(i, r, g, b);
+//  }
+//}
+
+byte WheelR(int WheelPos) {
   WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) return 255 - WheelPos * 3;
+  if (WheelPos < 85) {
+    return 255 - WheelPos * 3;
+  }
   if (WheelPos < 170) {
-    WheelPos -= 85;
     return 0;
   }
   WheelPos -= 170;
   return WheelPos * 3;
 }
 
-byte WheelG(byte WheelPos) {
+byte WheelG(int WheelPos) {
   WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) return 0;
+  if (WheelPos < 85) {
+    return 0;
+  }
   if (WheelPos < 170) {
     WheelPos -= 85;
     return WheelPos * 3;
@@ -132,9 +178,11 @@ byte WheelG(byte WheelPos) {
   return 255 - WheelPos * 3;
 }
 
-byte WheelB(byte WheelPos) {
+byte WheelB(int WheelPos) {
   WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) return WheelPos * 3;
+  if (WheelPos < 85) {
+    return WheelPos * 3;
+  }
   if (WheelPos < 170) {
     WheelPos -= 85;
     return 255 - WheelPos * 3;
@@ -148,34 +196,34 @@ byte WheelB(byte WheelPos) {
 
 
 void receivedCallback(uint32_t from, String & msg) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  //Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
   // Reset blink task
   onFlag = false;
 
-  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  //Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
 void changedConnectionCallback() {
-  Serial.printf("Changed connections %s\n", mesh.subConnectionJson().c_str());
+  //Serial.printf("Changed connections %s\n", mesh.subConnectionJson().c_str());
   // Reset blink task
   onFlag = false;
 
   nodes = mesh.getNodeList();
-  Serial.printf("Num nodes: %d\n", nodes.size());
-  Serial.printf("Connection list:");
+  //Serial.printf("Num nodes: %d\n", nodes.size());
+  //Serial.printf("Connection list:");
   SimpleList<uint32_t>::iterator node = nodes.begin();
   while (node != nodes.end()) {
-    Serial.printf(" %u", *node);
+    //Serial.printf(" %u", *node);
     node++;
   }
   Serial.println();
   calc_delay = true;
 }
 void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+  //Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
 void delayReceivedCallback(uint32_t from, int32_t delay) {
-  Serial.printf("Delay to node %u is %d us\n", from, delay);
+  //Serial.printf("Delay to node %u is %d us\n", from, delay);
 }
